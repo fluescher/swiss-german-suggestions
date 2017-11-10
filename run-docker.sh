a122493cef937beccd40dc03f6a4ec51e800562e
taabusaa <samuel.bucheli@swissom.com>
2017-11-10 09:52:16 +0100

    Some additional experimenting

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,6 @@ IMAGE_NAME="keras-jupyter"
 
 docker build -t ${IMAGE_NAME} .
 
-docker run --rm -v ${PWD}/notebooks:/notebooks -p 8888:8888 ${IMAGE_NAME}
+CONTAINER_ID=$(docker run -d -it --rm -v ${PWD}/notebooks:/notebooks -p 6006:6006 -p 8888:8888 ${IMAGE_NAME})I
+
+docker logs ${CONTAINER_ID}
